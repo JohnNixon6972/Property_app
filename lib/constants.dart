d04383aa3bd71798c9154ce310b8996531bd682f
John Nixon <69578073+JohnNixon6972@users.noreply.github.com>
2022-04-23 12:52:30 +0530

    Made some final UI changes

--- lib/constants.dart
+++ lib/constants.dart
@@ -46,16 +46,6 @@ const kTextFieldDecoration = InputDecoration(
 // const kHighlightedTextColor = Color(0xff5F9FFE);
 // const kNavigationIconColor = Color.fromARGB(255, 188, 188, 188);
 
-// // Orange Color Scheme
-// const kPageBackgroundColor = Color(0xffF5DEB4);
-// const kSubCategoryColor = Color.fromARGB(255, 151, 151, 151);
-// const kPrimaryButtonColor = Color(0xffC78024);
-// const kSecondaryButtonColor = Color.fromARGB(255, 236, 236, 236);
-// const kHighlightedTextColor = Color(0xff5F9FFE);
-// const kNavigationIconColor = Color.fromARGB(255, 188, 188, 188);
-// const kBottomNavigationBackgroundColor = Color(0xffD7AC81);
-// const kPropertyCardColor = Color(0xffFCB07A);
-
 // Beige Color Scheme
 // const kPageBackgroundColor = Color(0xffECE8E0);
 // const kSubCategoryColor = Color.fromARGB(255, 151, 151, 151);
@@ -66,16 +56,25 @@ const kTextFieldDecoration = InputDecoration(
 // const kBottomNavigationBackgroundColor = Colors.white;
 // const kPropertyCardColor = Colors.white;
 // const kTextFieldFillColor = Color.fromARGB(255, 226, 223, 218);
-
-const kPageBackgroundColor = Color.fromARGB(255, 238, 244, 248);
+const kPageBackgroundColor = Color(0xffE9E3E3);
 const kSubCategoryColor = Color.fromARGB(255, 151, 151, 151);
-const kPrimaryButtonColor = Color(0xff4B6274);
-const kSecondaryButtonColor = Color.fromARGB(255, 236, 236, 236);
-const kHighlightedTextColor = Color(0xff4B6274);
-const kNavigationIconColor = Color.fromARGB(255, 188, 188, 188);
-const kBottomNavigationBackgroundColor = Color.fromARGB(255, 238, 244, 248);
-const kPropertyCardColor = Color.fromARGB(255, 151, 151, 151);
-const kTextFieldFillColor = Color.fromARGB(255, 226, 223, 218);
+const kPrimaryButtonColor = Color(0xffF510055);
+const kSecondaryButtonColor = Color(0xffDAD8D9);
+const kHighlightedTextColor = Color(0xff510055);
+const kNavigationIconColor = Color.fromARGB(255, 211, 199, 199);
+const kBottomNavigationBackgroundColor = Color.fromARGB(255, 190, 157, 170);
+const kPropertyCardColor = Color(0xffDAD8D9);
+const kTextFieldFillColor = Color.fromARGB(255, 211, 199, 199);
+
+// const kPageBackgroundColor = Color.fromARGB(255, 238, 244, 248);
+// const kSubCategoryColor = Color.fromARGB(255, 151, 151, 151);
+// const kPrimaryButtonColor = Color(0xff4B6274);
+// const kSecondaryButtonColor = Color.fromARGB(255, 236, 236, 236);
+// const kHighlightedTextColor = Color(0xff4B6274);
+// const kNavigationIconColor = Color.fromARGB(255, 188, 188, 188);
+// const kBottomNavigationBackgroundColor = Color.fromARGB(255, 238, 244, 248);
+// const kPropertyCardColor = Color.fromARGB(255, 151, 151, 151);
+// const kTextFieldFillColor = Color.fromARGB(255, 226, 223, 218);
 
 // // Green Color Scheme
 // const kPageBackgroundColor = Color(0xffD6F3E1);
--- lib/screens/addPopertiesScreen2.dart
+++ lib/screens/addPopertiesScreen2.dart
@@ -1,6 +1,7 @@
 import '../constants.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:dotted_border/dotted_border.dart';
 
 class AddPropertiesScreen2 extends StatefulWidget {
   static const String id = 'addPropertiesScreen2';
@@ -9,54 +10,273 @@ class AddPropertiesScreen2 extends StatefulWidget {
 }
 
 class _AddPropertiesScreen2State extends State<AddPropertiesScreen2> {
+  var _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kPageBackgroundColor,
       body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.all(14.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Transform.rotate(
-              angle: 270 * pi / 180,
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.pop(context);
-                },
-                child: Icon(
-                  Icons.expand_less_rounded,
-                  size: 40,
+        child: Padding(
+          padding: const EdgeInsets.all(14.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Transform.rotate(
+                angle: 270 * pi / 180,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Icon(
+                    Icons.expand_less_rounded,
+                    size: 40,
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 20, vertical: 10.0),
-              child: Row(
-                children: [
-                  Text(
-                    'Add Properties',
-                    style: TextStyle(
-                        color: kHighlightedTextColor,
-                        fontSize: 28,
-                        fontWeight: FontWeight.w700),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10.0),
+                child: Row(
+                  children: [
+                    Text(
+                      'Add Properties',
+                      style: TextStyle(
+                          color: kHighlightedTextColor,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w700),
+                    ),
+                    Spacer(),
+                    Text(
+                      'Step 2/2',
+                      style: TextStyle(
+                          color: kSubCategoryColor,
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 12),
+                child: DottedBorder(
+                  color: kHighlightedTextColor,
+                  radius: Radius.circular(20),
+                  borderType: BorderType.RRect,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: kPropertyCardColor,
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 12.0, horizontal: 14),
+                      child: Row(
+                        children: [
+                          Text(
+                            'Upload Image',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500, fontSize: 18),
+                          ),
+                          Spacer(),
+                          CircleAvatar(
+                            backgroundColor: kPageBackgroundColor,
+                            child: Icon(
+                              Icons.photo_library_sharp,
+                              color: kHighlightedTextColor,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
                   ),
-                  Spacer(),
-                  Text(
-                    'Step 1/2',
-                    style: TextStyle(
-                        color: kSubCategoryColor,
-                        fontSize: 15,
-                        fontWeight: FontWeight.w500),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 5),
+                child: Container(
+                  child: Row(
+                    children: [
+                      ImagesFromGallery(img_url: 'images/property_img1.jpg'),
+                      ImagesFromGallery(img_url: 'images/property_img2.jpg'),
+                      ImagesFromGallery(img_url: 'images/property_img3.jpg'),
+                      Spacer()
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 14),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: kHighlightedTextColor),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Write Property Description',
+                          style: TextStyle(color: kSubCategoryColor),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(border: InputBorder.none),
+                          controller: _controller,
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                          maxLines: 3,
+                          // minLines: 5,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      PropertyDetailTile(HintText: "Square Fit"),
+                      Spacer(),
+                      PropertyDetailTile(
+                        HintText: "Bed Room",
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      PropertyDetailTile(HintText: "Bathroom"),
+                      Spacer(),
+                      PropertyDetailTile(
+                        HintText: "Price (USD)",
+                      )
+                    ],
                   )
                 ],
               ),
-            ),
-          ],
+              Spacer(),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 25.0),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        height: 70,
+                        width: 160,
+                        decoration: BoxDecoration(
+                          color: kNavigationIconColor,
+                          borderRadius: BorderRadius.circular(35),
+                        ),
+                        child: Center(
+                          child: Text('Preview',
+                              style: TextStyle(
+                                  color: kHighlightedTextColor,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w400)),
+                        ),
+                      ),
+                    ),
+                    Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        height: 70,
+                        width: 160,
+                        decoration: BoxDecoration(
+                          color: kHighlightedTextColor,
+                          borderRadius: BorderRadius.circular(35),
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Submit',
+                            style: TextStyle(
+                                color: kSubCategoryColor,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w400),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
-      )),
+      ),
+    );
+  }
+}
+
+class PropertyDetailTile extends StatelessWidget {
+  final String HintText;
+  PropertyDetailTile({required this.HintText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: 70,
+        width: 160,
+        decoration: BoxDecoration(
+          border: Border.all(color: kHighlightedTextColor),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            keyboardType: TextInputType.number,
+            maxLines: 1,
+            cursorColor: kHighlightedTextColor,
+            textAlign: TextAlign.left,
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: HintText,
+              hintStyle: TextStyle(fontSize: 17, fontWeight: FontWeight.w600),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ImagesFromGallery extends StatelessWidget {
+  final String img_url;
+  ImagesFromGallery({required this.img_url});
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 10.0),
+      child: Stack(
+        overflow: Overflow.visible,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+            child: Image(
+              image: AssetImage(img_url),
+              height: 80,
+              fit: BoxFit.fill,
+              width: 80,
+            ),
+          ),
+          Positioned(
+              left: 60,
+              top: -5,
+              child: Icon(
+                Icons.cancel,
+                color: kHighlightedTextColor,
+                size: 25,
+              ))
+        ],
+      ),
     );
   }
 }

99192b8d8f1dc876e9681b42cb4572e98ca1a39f
Hrutuja Patnekar <hrutujapatnekar22@gmail.com>
2022-04-25 10:49:45 +0530

    Updated main branch

--- lib/constants.dart
+++ lib/constants.dart
@@ -57,25 +57,25 @@ const kTextFieldDecoration = InputDecoration(
 // const kPropertyCardColor = Color(0xffFCB07A);
 
 // Beige Color Scheme
-// const kPageBackgroundColor = Color(0xffECE8E0);
-// const kSubCategoryColor = Color.fromARGB(255, 151, 151, 151);
-// const kPrimaryButtonColor = Color.fromARGB(255, 9, 70, 32);
-// const kSecondaryButtonColor = Color.fromARGB(255, 236, 236, 236);
-// const kHighlightedTextColor = Color.fromARGB(255, 9, 70, 32);
-// const kNavigationIconColor = Color.fromARGB(255, 188, 188, 188);
-// const kBottomNavigationBackgroundColor = Colors.white;
-// const kPropertyCardColor = Colors.white;
-// const kTextFieldFillColor = Color.fromARGB(255, 226, 223, 218);
-
-const kPageBackgroundColor = Color.fromARGB(255, 238, 244, 248);
+const kPageBackgroundColor = Color(0xffECE8E0);
 const kSubCategoryColor = Color.fromARGB(255, 151, 151, 151);
-const kPrimaryButtonColor = Color(0xff4B6274);
+const kPrimaryButtonColor = Color.fromARGB(255, 9, 70, 32);
 const kSecondaryButtonColor = Color.fromARGB(255, 236, 236, 236);
-const kHighlightedTextColor = Color(0xff4B6274);
+const kHighlightedTextColor = Color.fromARGB(255, 9, 70, 32);
 const kNavigationIconColor = Color.fromARGB(255, 188, 188, 188);
-const kBottomNavigationBackgroundColor = Color.fromARGB(255, 238, 244, 248);
-const kPropertyCardColor = Color.fromARGB(255, 151, 151, 151);
+const kBottomNavigationBackgroundColor = Colors.white;
+const kPropertyCardColor = Colors.white;
 const kTextFieldFillColor = Color.fromARGB(255, 226, 223, 218);
+
+// const kPageBackgroundColor = Color.fromARGB(255, 238, 244, 248);
+// const kSubCategoryColor = Color.fromARGB(255, 151, 151, 151);
+// const kPrimaryButtonColor = Color(0xff4B6274);
+// const kSecondaryButtonColor = Color.fromARGB(255, 236, 236, 236);
+// const kHighlightedTextColor = Color(0xff4B6274);
+// const kNavigationIconColor = Color.fromARGB(255, 188, 188, 188);
+// const kBottomNavigationBackgroundColor = Color.fromARGB(255, 238, 244, 248);
+// const kPropertyCardColor = Color.fromARGB(255, 151, 151, 151);
+// const kTextFieldFillColor = Color.fromARGB(255, 226, 223, 218);
 
 // // Green Color Scheme
 // const kPageBackgroundColor = Color(0xffD6F3E1);

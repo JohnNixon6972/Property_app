0bcf3af14cb81c2be57c0cdb67d55c68cba85308
John Nixon <johnbrightnixon@gmail.com>
2022-10-11 21:35:49 +0530

    Version 2

--- lib/components/addressPickerHelper.dart
+++ lib/components/addressPickerHelper.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_typing_uninitialized_variables, must_be_immutable
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geocode/geocode.dart';
@@ -10,7 +12,7 @@ late var _origin = null;
 Address address = Address();
 
 class PinAddressMap extends StatefulWidget {
-  PinAddressMap({
+  const PinAddressMap({
     Key? key,
   }) : super(key: key);
 
@@ -47,8 +49,8 @@ class _PinAddressMapState extends State<PinAddressMap> {
                           latitude: _origin.position.latitude,
                           longitude: _origin.position.longitude);
                       setState(() {
-                        print(address.streetAddress);
-                        print(address.region);
+                        // print(address.streetAddress);
+                        // print(address.region);
                         PropertyAddress = address.streetAddress!;
                         EPropertyAddress = address.streetAddress!;
                         latitude = _origin.position.latitude.toString();
